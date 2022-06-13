ec52bb2488df3d41c13a8674ead19b13477ddb1a
Anthony <aezeji00@gmail.com>
2022-06-12 21:38:02 -0400

    commit

--- frontend/src/js/Messages.js
+++ frontend/src/js/Messages.js
@@ -7,25 +7,26 @@ import {db} from './Firebase'
 import {query,where, onSnapshot, collection, addDoc, getDocs, orderBy } from "firebase/firestore";
 
 function Messages() {
-    try {
-        var otherUsersId = []
-        var uniqueIds = []
-        var params = useParams()
-        
-        
-        var navigate = useNavigate()
-        const [messages, setMessages] = useState([])
-        const [messagesList, setMessagesList] = useState([])
-        var arr = []
-        var objectToPush = []
-        var data = {}
-        var messagesArr = []
-      
-        var tempArr = []
-        var currUser = {}
-        useEffect(() => {
+
+    var otherUsersId = []
+    var uniqueIds = []
+    var params = useParams()
+    
+    
+    var navigate = useNavigate()
+    const [messages, setMessages] = useState([])
+    const [messagesList, setMessagesList] = useState([])
+    var arr = []
+    var objectToPush = []
+    var data = {}
+    var messagesArr = []
+  
+    var tempArr = []
+    var currUser = {}
+    useEffect(() => {
+        try {
             async function getMessages(){
-             
+         
                 function compareMessages(currentElement){
                    
                      if(currentElement.data().sentTo==JSON.parse(window.sessionStorage.getItem('session')).user.id||currentElement.data().sentFrom==JSON.parse(window.sessionStorage.getItem('session')).user.id){   
@@ -51,76 +52,77 @@ function Messages() {
                     console.log(snapshot.docs)
                     setMessages(snapshot.docs.sort(compareTime).filter(compareMessages).map(doc=>{return doc.data()})) 
                 })
+                return () => {
+                    getMessages()
+                  }
             }
-            
-       
-          
-          return () => {
-            getMessages()
-          }
-        }, [])
-    
-        useEffect(() => {
-      console.log(messages)
-           tempArr = []
-           
-           messages.forEach(doc=>{
-               var data = doc
-               if(data.sentFrom == JSON.parse(window.sessionStorage.getItem('session') ).user.id){
-                   tempArr.push(data.sentTo)
-                   
-               }else{
-                   tempArr.push(data.sentFrom)
-               }
-           })
-        var newSet = [...new Set(tempArr)]
-       
-            newSet.forEach(async id=>{
-                var userMessages = []
-                currUser = {}
-                await axios.get(`https://3.92.186.223:5000/api/users/2/${id}`).then(res=>{
-                   currUser = res.data
-                    console.log(res.data)
-                })
-                messages.forEach(doc=>{
-                    
-                    if(doc.sentTo == id||doc.sentFrom==id){
-                        
-                        userMessages.push(doc)
-                       
-                    }
-                })
-               
-              arr.push({data:userMessages, currUser})
-              setMessagesList(arr.map((doc)=>doc))
-            })
-    
-        
-          return () => {
-            
-          }
-        }, [messages])
-        
-     
-     
-        async function handleMessageClick(e){
-            console.log(messagesList)
-    for(var i = 0 ; i < messagesList.length;i++){
-    
-    
-        if(messagesList[i].currUser.userName == e.target.parentNode.firstChild.innerText ){
-        
-            await axios.get(`https://3.92.186.223:5000/api/users/${e.target.parentNode.firstChild.innerText.toString()}`).then(res=>{
-                window.sessionStorage.setItem('currRec', JSON.stringify(res.data))
-            })
-            navigate(`/chat/${e.target.parentNode.firstChild.innerText.toString()}`)
+        } catch (error) {
+            console.log(error)
         }
-    }
-        }
-    } catch (error) {
-        console.log(err)
-    }
+       
+        
    
+      
+     
+    }, [])
+
+    useEffect(() => {
+  console.log(messages)
+       tempArr = []
+       
+       messages.forEach(doc=>{
+           var data = doc
+           if(data.sentFrom == JSON.parse(window.sessionStorage.getItem('session') ).user.id){
+               tempArr.push(data.sentTo)
+               
+           }else{
+               tempArr.push(data.sentFrom)
+           }
+       })
+    var newSet = [...new Set(tempArr)]
+   
+        newSet.forEach(async id=>{
+            var userMessages = []
+            currUser = {}
+            await axios.get(`https://3.92.186.223:5000/api/users/2/${id}`).then(res=>{
+               currUser = res.data
+                console.log(res.data)
+            })
+            messages.forEach(doc=>{
+                
+                if(doc.sentTo == id||doc.sentFrom==id){
+                    
+                    userMessages.push(doc)
+                   
+                }
+            })
+           
+          arr.push({data:userMessages, currUser})
+          setMessagesList(arr.map((doc)=>doc))
+        })
+
+    
+      return () => {
+        
+      }
+    }, [messages])
+    
+ 
+ 
+    async function handleMessageClick(e){
+        console.log(messagesList)
+for(var i = 0 ; i < messagesList.length;i++){
+
+
+    if(messagesList[i].currUser.userName == e.target.parentNode.firstChild.innerText ){
+    
+        await axios.get(`https://3.92.186.223:5000/api/users/${e.target.parentNode.firstChild.innerText.toString()}`).then(res=>{
+            window.sessionStorage.setItem('currRec', JSON.stringify(res.data))
+        })
+        navigate(`/chat/${e.target.parentNode.firstChild.innerText.toString()}`)
+    }
+}
+    }
   return (
     <div className='messages'>
     <h3>Messages</h3>
